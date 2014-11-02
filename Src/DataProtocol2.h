2895f3a2e4d3067c40ae8d177ec99cd38a56d2a2
Donald Pillou <don.pillou@gmail.com>
2014-11-02 10:45:59 +0100

    WIP

--- Src/DataProtocol2.h
+++ Src/DataProtocol2.h
@@ -52,7 +52,7 @@ public:
   {
     uint8_t flags; // e.g. PUBLIC, 
   };
---
+
   struct SubscribeRequest
   {
     char_t channel[33];
@@ -61,91 +61,18 @@ public:
   };
   struct SubscribeResponse
   {
-    char_t channel[33];
     uint64_t channelId;
-    uint32_t flags;
+    //uint32_t flags;
   };
   struct UnsubscribeRequest
   {
-    char_t channel[33];
+    uint32_t channelId;
   };
   struct UnsubscribeResponse
   {
-    char_t channel[33];
-    uint64_t channelId;
+    uint32_t channelId;
   };
 
-  struct RegisterSourceRequest
-  {
-    char_t channel[33];
-  };
-  struct RegisterSourceResponse
-  {
-    char_t channel[33];
-    uint64_t channelId;
-  };
-
-  struct RegisterSinkRequest
-  {
-    char_t channel[33];
-  };
-  struct RegisterSinkResponse
-  {
-    char_t channel[33];
-    uint64_t channelId;
-  };
-
-  struct TradeRequest
-  {
-    uint64_t maxAge;
-    uint64_t sinceId;
-  };
-  struct Trade
-  {
-    uint64_t id;
-    uint64_t time;
-    double price;
-    double amount;
-    uint32_t flags;
-  };
-  struct TradeMessage
-  {
-    uint64_t channelId;
-    Trade trade;
-  };
-  struct TimeMessage
-  {
-    uint64_t channelId;
-    uint64_t time;
-  };
-  struct TradeResponse
-  {
-    uint64_t channelId;
-  };
-  struct Channel
-  {
-    char_t channel[33];
-    //uint8_t type;
-    //uint32_t flags;
-  };
-
-  struct TimeResponse
-  {
-    uint64_t time;
-  };
-
-  struct Ticker
-  {
-    uint64_t time;
-    double bid;
-    double ask;
-  };
-
-  struct TickerMessage
-  {
-    uint64_t channelId;
-    Ticker ticker;
-  };
 
 #pragma pack(pop)
 
